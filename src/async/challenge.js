329ab176e579831cd6300b1f27f544d3215a20bf
juliol1 <juliovillalvazo26@gmail.com>
2021-09-23 20:00:35 -0500

    terminacion del curso de asincronismo

---
+++ src/async/challenge.js
@@ -0,0 +1,23 @@
+const fetchData = require('../utils/fetchData');
+
+// esta en mayusculas por convecion de que si algo nunca se va a mover
+// o nunca va a cambiar
+const API = 'https://rickandmortyapi.com/api/character/';
+
+const anotherFunction = async (url_api) => {
+    try {
+        const data = await fetchData(url_api);
+        const character = await fetchData(`${API}${data.results[0].id}`);
+        const origin = await fetchData(character.origin.url);
+
+        console.log(data.info.count);
+        console.log(character.name);
+        console.log(origin.dimension);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+console.log('Before');
+anotherFunction(API);
+console.log('After');
